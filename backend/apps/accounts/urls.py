2e660aaa0c3c36ce124b16343419235642f2b41e
rubanaa <andreygrigorev0@gmail.com>
2024-07-31 11:33:13 +0300

    setting apps accounts

---
+++ backend/apps/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path("grades/", views.StudentGrades.as_view(), name="student_grades"),
+]
--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from .views import set_csrf
+from apps.accounts import urls as students_urls
 
 urlpatterns = [
     path("api/csrf/", set_csrf, name="csrf"),
