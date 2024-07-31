01f5ea99af4de4038187841c64980f0150dea194
rubanaa <andreygrigorev0@gmail.com>
2024-07-31 11:45:52 +0300

    init desk app

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from .views import set_csrf
 from apps.accounts import urls as accounts_urls
 from apps.work_space import urls as work_space_urls
+from apps.desk import urls as desk_urls
+
 
 urlpatterns = [
     path("api/csrf/", set_csrf, name="csrf"),
