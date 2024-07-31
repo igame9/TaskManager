1bfb1ab7b266a810c6b8f04c7c5bbcfa9b77df39
rubanaa <andreygrigorev0@gmail.com>
2024-07-31 11:40:09 +0300

    init work_space app

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from .views import set_csrf
-from apps.accounts import urls as students_urls
+from apps.accounts import urls as accounts_urls
+from apps.work_space import urls as work_space_urls
 
 urlpatterns = [
     path("api/csrf/", set_csrf, name="csrf"),
---
+++ backend/apps/work_space/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WorkSpaceConfig(AppConfig):
+    name = 'apps.work_space'
