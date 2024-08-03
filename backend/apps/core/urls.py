abe3a3aeeffe0548eca483bf8102fed9f162d2f8
Andrey <andreygrigorev0@gmail.com>
2024-08-03 20:21:06 +0300

    change url.py

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -3,7 +3,7 @@ from .views import set_csrf
 from apps.work_space import urls as work_space_urls
 
 urlpatterns = [
-    path("api/csrf/", set_csrf, name="csrf"),
-    path("api/work_space/", include(work_space_urls)),
+    path("csrf/", set_csrf, name="csrf"),
+    path("work_space/", include(work_space_urls)),
 
 ]
